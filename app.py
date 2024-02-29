e09e32ab0df6c095f154eb6f254d71541d416ad4
R. YPeng <saaraas_rypeng@yeah.net>
2024-02-29 22:34:20 +0800

    Update app.py

--- app.py
+++ app.py
@@ -2,6 +2,8 @@ import streamlit as st
 
 ana_url = "https://while-nalu-secretary-ana.hf.space"
 # 使用st.iframe函数显示外部Streamlit应用程序
-st.iframe(app_url, 
-	  width=850, 
-	  height=450)
+st.iframe(
+	app_url, 
+	width=850, 
+	height=450
+)
