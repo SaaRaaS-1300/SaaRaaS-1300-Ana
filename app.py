750074087a0110beb601889b526af4ea4e0238bc
R. YPeng <saaraas_rypeng@yeah.net>
2024-02-29 22:33:36 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,15 +1,7 @@
 import streamlit as st
 
-
-# HTML内容
-html_content = """
-<iframe
-	src="https://while-nalu-secretary-ana.hf.space"
-	frameborder="0"
-	width="850"
-	height="450"
-></iframe>
-"""
- 
-# 将HTML内容传递给st.markdown()函数进行展示
-st.markdown(html_content)
+ana_url = "https://while-nalu-secretary-ana.hf.space"
+# 使用st.iframe函数显示外部Streamlit应用程序
+st.iframe(app_url, 
+	  width=850, 
+	  height=450)
